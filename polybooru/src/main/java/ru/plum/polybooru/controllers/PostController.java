ce3a8a8a8938a58e3e6799e7182d5a161fb65d64
PlumBoom82 <adusha43@gmail.com>
2023-09-18 20:43:38 +0300

    commit

--- polybooru/src/main/java/ru/plum/polybooru/controllers/PostController.java
+++ polybooru/src/main/java/ru/plum/polybooru/controllers/PostController.java
@@ -1,20 +1,17 @@
 package ru.plum.polybooru.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.plum.polybooru.dao.PostDAO;
 import ru.plum.polybooru.models.Post;
 
-//@Controller
-@RestController
+@Controller
 public class PostController {
 
     @GetMapping("/")
-    public ResponseEntity hello() {
-        return new ResponseEntity<>("{}", HttpStatusCode.valueOf(200));
+    public String hello() {
+        return "test";
     }
 }
